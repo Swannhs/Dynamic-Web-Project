daae98b33e881ab63c4e84fc40b268f28c408613
Swannhs <princeapurbask@gmail.com>
2020-06-07 02:43:07 +0600

    Push in github

---
+++ src/main/java/com/swann/controller/HomeController.java
@@ -0,0 +1,34 @@
+package com.swann.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String home() {
+		return "index";
+	}
+
+	@RequestMapping("/index")
+	public String index(){
+		return "index";
+	}
+	@RequestMapping("/blogslist")
+	public String blogslist(){
+		return "blogslist";
+	}
+
+	@RequestMapping("/about")
+	public String about(){
+		return "about";
+	}
+	@RequestMapping("/login")
+	public String login(){
+		return "login";
+	}
+	@RequestMapping("signup")
+	public String registration(){
+		return "registration";
+	}
+}
